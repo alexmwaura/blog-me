87cead7f5b6d5a5a36bd4a1a7938fc945d065f7a
alexmwaura <alexmwaura43@gmail.com>
2022-04-07 16:20:59 +0300

    create article list component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule
